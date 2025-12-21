0cbb569eb493718295e799e47c0b22ef2e3af244
Anthony Martinez <155309360+martinezanthony@users.noreply.github.com>
2025-12-20 21:34:21 -0300

    Determinación de who mejorada.

--- plugins/owner-rd.js
+++ plugins/owner-rd.js
@@ -6,11 +6,19 @@ plugin.onlyOwner = true;
 
 plugin.run = async (m, { client, text, usedPrefix, command }) => {
   let who;
-  const numberMatches = text.match(/@[0-9]+/g);
-  if (numberMatches && numberMatches.length > 0) {
-    who = numberMatches[0].replace("@", "") + "@lid";
-  } else {
-    who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : null;
+  const numberMatches = text.match(/@[0-9\s]+/g);
+  const numberMatchesPlus = text.match(/\+[0-9\s]+/g);
+  if (numberMatchesPlus && numberMatchesPlus.length > 0) {
+    who = numberMatchesPlus[0].replace(/[+\s]/g, "") + "@s.whatsapp.net";
+  } else if (numberMatches && numberMatches.length > 0) {
+    who = numberMatches[0].replace("@", "").replace(/\s+/g, "") + "@lid";
+  } else if (m.quoted) {
+    who = m.quoted.sender;
+  }
+
+  if (!who.endsWith("@lid")) {
+    const whoData = getUser(who);
+    who = whoData?.lid || null;
   }
 
   if (!who) return client.sendText(m.chat, txt.defaultWho(usedPrefix, command), m);
@@ -19,7 +27,7 @@ plugin.run = async (m, { client, text, usedPrefix, command }) => {
   const ownerJids = globalThis.owners.map((owner) => owner + "@s.whatsapp.net");
   for (const ownerJid of ownerJids) {
     const ownerData = getUser(ownerJid);
-    if (who == ownerData?.lid) return m.react("❌");
+    if (who == ownerData?.lid && m.sender !== who) return m.react("❌");
   }
 
   // eliminar usuario de la db.
